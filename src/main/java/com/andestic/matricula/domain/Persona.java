c756f5a971b5e68ac1f1b335b11e7e2ac4078bf1
jvillane <jvillane@gmail.com>
2015-03-31 18:21:53 -0300

    hola

---
+++ src/main/java/com/andestic/matricula/domain/Persona.java
@@ -0,0 +1,31 @@
+package com.andestic.matricula.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by jvillane on 31-03-15.
+ */
+@Entity
+@Table(name = "persona")
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    long id;
+    String nombre;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
